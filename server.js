1367f6c8d22772f9115258e9232e06d6f31bac0a
ColeTrammer <cole.trammer@gmail.com>
2017-05-22 16:15:49 -0700

    Added ip address

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-
+    const response = {};
+    response.ipaddress = req.ip;
+    res.send(response);
 })
 
 app.listen(process.env.PORT || 3000, () => {
