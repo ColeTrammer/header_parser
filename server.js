8e738336d8ff90d9cc1d5bc18432469761cda8fa
ColeTrammer <cole.trammer@gmail.com>
2017-05-22 16:03:10 -0700

    updated package.json

--- server.js
+++ server.js
@@ -6,4 +6,6 @@ app.get("/", (req, res) => {
 
 })
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Connected to port: ${process.env.PORT || 3000}`);
+});
