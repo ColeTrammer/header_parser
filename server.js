4e4dc3adcb331512288673d4b1a0cb8fd0b17022
ColeTrammer <cole.trammer@gmail.com>
2017-05-22 17:09:49 -0700

    added more info to response

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const app = express();
 app.get("/", (req, res) => {
     const response = {};
     response.ipaddress = req.ip.includes("::ffff:") ? req.ip.substr(7) : req.ip;
+    response.language = req.get("accept-language").split(",")[0];
+    const softwareInfo = req.get("user-agent").match(/\(.+\)/g)[0];
+    response.software = softwareInfo.substr(1, softwareInfo.length - 2);
     res.send(response);
-})
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Connected to port: ${process.env.PORT || 3000}`);
